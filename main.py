2eaebfc4d9a26781662e274bd769dd30ba1def7d
CheeseB0y <cheeseb0y1316@gmail.com>
2025-09-29 00:06:59 -0400

    More user interface improvements.

--- main.py
+++ main.py
@@ -13,9 +13,17 @@ class PokerClock:
     def __init__(self, rounds):
         self.root = tk.Tk()
         self.rounds = rounds
+        self.round_index = 0
+        self.round_num = tk.StringVar(value=f"Round: {rounds[self.round_index].num}")
+        self.b_blind = tk.StringVar(value=f"Big Blind: {rounds[self.round_index].b_blind:,}")
+        self.s_blind = tk.StringVar(value=f"Small Blind: {rounds[self.round_index].s_blind:,}")
+        self.time_remaining = rounds[self.round_index].time * 60
+        self.time_display = tk.StringVar(value=self.format_time_remaining())
 
         self.root.title("Poker Clock")
         self.root.geometry("1200x900")
+        self.root.configure(bg="green")
+
         
         menubar = tk.Menu(self.root)
         self.root.config(menu=menubar)
@@ -26,38 +34,32 @@ class PokerClock:
         option_menu.add_command(label="Edit Game")
         option_menu.add_command(label="Exit", command=quit)
 
-        self.round_index = 0
-        self.round_num = tk.StringVar(value=f"Round: {rounds[self.round_index].num}")
-        self.b_blind = tk.StringVar(value=f"Big Blind: {rounds[self.round_index].b_blind:,}")
-        self.s_blind = tk.StringVar(value=f"Small Blind: {rounds[self.round_index].s_blind:,}")
-
-        self.time_remaining = rounds[self.round_index].time * 60
-        self.time_display = tk.StringVar(value=self.format_time_remaining())
-
         self.root.columnconfigure(0, weight=1)
         self.root.columnconfigure(1, weight=1)
         self.root.columnconfigure(2, weight=1)
-        self.root.columnconfigure(3, weight=1)
-        self.root.columnconfigure(4, weight=1)
-        self.root.columnconfigure(5, weight=1)
+        
+        round_frame = tk.Frame(self.root, pady=10, bg="black")
+        round_frame.grid(row=0, column=0, columnspan=3, sticky="NESW")
+        round_frame.columnconfigure(0, weight=1)
+        round_frame.columnconfigure(1, weight=1)
+        round_frame.columnconfigure(2, weight=1)
 
-        top_menu_box = tk.Frame(self.root, bg='red')
-        top_menu_box.grid(row=0, column=1, sticky='WE')
-        time_frame = tk.Frame(self.root, bg='green')
-        time_frame.grid(row=1, column=1, sticky='WE')
-
-        round_number_label = tk.Label(top_menu_box, textvariable=self.round_num).grid(row=0, column=1, padx=10, pady=10, sticky='NSWE')
-        timer_label = tk.Label(time_frame, textvariable=self.time_display).grid(row=1, column=1, padx=10, pady=10, sticky='NSWE')
-        timer_button = tk.Button(self.root, text="Start timer", command=self.start_timer).grid(row=1, column=2, padx=10, pady=10, sticky='NSWE')
-        s_blind_label = tk.Label(self.root, textvariable=self.s_blind).grid(row=2, column=1, padx=10, pady=10, sticky='NSWE')
-        b_blind_label = tk.Label(self.root, textvariable=self.b_blind).grid(row=2, column=2, padx=10, pady=10, sticky='NSWE')
-        next_button = tk.Button(self.root, text="Next round", command=self.next_round).grid(row=2, column=3, padx=10, pady=10, sticky='NSWE')
+        round_number_label = tk.Label(round_frame, textvariable=self.round_num, bg="black", fg="white").grid(row=0, column=1, sticky='NESW')
+        timer_label = tk.Label(self.root, textvariable=self.time_display, bg="green", fg="white", font={"Arial", 64}).grid(row=1, column=0, columnspan=2, rowspan=2, sticky='NESW')
+        timer_button = tk.Button(self.root, text="Start timer", command=self.start_timer, bg="red", fg="white").grid(row=1, column=2, sticky='NESW')
+        s_blind_label = tk.Label(self.root, textvariable=self.s_blind, bg="red", fg="white").grid(row=3, column=0, sticky='NESW')
+        b_blind_label = tk.Label(self.root, textvariable=self.b_blind, bg="black", fg="white").grid(row=3, column=1, sticky='NESW')
+        next_button = tk.Button(self.root, text="Next round", command=self.next_round, bg="black", fg="white").grid(row=2, column=2, sticky='NESW')
+        blank_corner = tk.Label(self.root, bg="red").grid(row=3, column=2, sticky="NESW")
 
         self.root.mainloop()
 
     def start_timer(self):
         self.pause = False
         self.countdown()
+
+    def pause_timer(self):
+        self.pause = True
 
     def countdown(self):
         if self.time_remaining > 0 and self.pause == False:
@@ -106,25 +108,25 @@ class PokerClock:
         game_editor_window.columnconfigure(4, weight=1)
         game_editor_window.columnconfigure(5, weight=1)
 
-        round_column = tk.Frame(game_editor_window, bg="red", padx=10, pady=10)
+        round_column = tk.Frame(game_editor_window, bg="red")
         round_column.grid(row=0, column=1, sticky='NSWE')
-        time_column = tk.Frame(game_editor_window, bg="black", padx=10, pady=10)
+        time_column = tk.Frame(game_editor_window, bg="black")
         time_column.grid(row=0, column=2, sticky='NSWE')
-        b_blind_column = tk.Frame(game_editor_window, bg="red", padx=10, pady=10)
+        b_blind_column = tk.Frame(game_editor_window, bg="red")
         b_blind_column.grid(row=0, column=3, sticky='NSWE')
-        s_blind_column = tk.Frame(game_editor_window, bg="black", padx=10, pady=10)
+        s_blind_column = tk.Frame(game_editor_window, bg="black")
         s_blind_column.grid(row=0, column=4, sticky='NSWE')
 
-        round_column_label = tk.Label(round_column, text="Round", bg="red", fg="white").grid(row=0, column=1, padx=10, pady=10, sticky='NSWE')
-        time_column_label = tk.Label(time_column, text="Time", bg="black", fg="white").grid(row=0, column=2, padx=10, pady=10, sticky='NSWE')
-        b_blind_column_label = tk.Label(b_blind_column, text="Big Blind", bg="red", fg="white").grid(row=0, column=3, padx=10, pady=10, sticky='NSWE')
-        s_blind_column_label = tk.Label(s_blind_column, text="Small Blind", bg="black", fg="white").grid(row=0, column=4, padx=10, pady=10, sticky='NSWE')
+        round_column_label = tk.Label(round_column, text="Round", bg="red", fg="white").grid(row=0, column=1, sticky='NESW')
+        time_column_label = tk.Label(time_column, text="Time", bg="black", fg="white").grid(row=0, column=2, sticky='NESW')
+        b_blind_column_label = tk.Label(b_blind_column, text="Big Blind", bg="red", fg="white").grid(row=0, column=3, sticky='NESW')
+        s_blind_column_label = tk.Label(s_blind_column, text="Small Blind", bg="black", fg="white").grid(row=0, column=4, sticky='NESW')
 
         for (index, round) in enumerate(self.rounds):
-            tk.Label(round_column, text=round.num, bg="red", fg="white").grid(row=index+1, column=1, padx=10, pady=10, sticky='NSWE')
-            tk.Label(time_column, text=round.time, bg="black", fg="white").grid(row=index+1, column=2, padx=10, pady=10, sticky='NSWE')
-            tk.Label(b_blind_column, text=round.b_blind, bg="red", fg="white").grid(row=index+1, column=3, padx=10, pady=10, sticky='NSWE')
-            tk.Label(s_blind_column, text=round.s_blind, bg="black", fg="white").grid(row=index+1, column=4, padx=10, pady=10, sticky='NSWE')
+            tk.Label(round_column, text=round.num, bg="red", fg="white").grid(row=index+1, column=1, sticky='NESW')
+            tk.Label(time_column, text=round.time, bg="black", fg="white").grid(row=index+1, column=2, sticky='NESW')
+            tk.Label(b_blind_column, text=round.b_blind, bg="red", fg="white").grid(row=index+1, column=3, sticky='NESW')
+            tk.Label(s_blind_column, text=round.s_blind, bg="black", fg="white").grid(row=index+1, column=4, sticky='NESW')
 
 if __name__ == '__main__':
     # Fix hard coded values later
